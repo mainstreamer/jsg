80204ca2e8b6733ac0bd16344d5712f56a9c5b4f
Artem Dolmatov <artem.kyiv@outlook.com>
2016-10-09 21:07:27 +0300

    snakie v0.1

--- snakie.js
+++ snakie.js
@@ -1,11 +1,36 @@
 var step = 50;
 var active = [];
-var body = [document.getElementById('user')];
+var body = [ new Cell(document.getElementById('user')) ];
 var wiggle = [];
 var info = {};
 
+function Cell(element) {
+    this.element = element;
+    this.direction = '';
+    this.cnt = 0;
+    this.index = 0;
+    // element: document.getElementById('user'),
+    this.moveRight = function () {
+        this.element.style.left = this.element.offsetLeft+1+'px'
+    };
+    this.moveLeft = function () {
+        this.element.style.left = this.element.offsetLeft-1+'px';
+    };
+    this.moveUp = function () {
+        this.element.style.top = this.element.offsetTop-1+'px';
+    };
+    this.moveDown = function () {
+        this.element.style.top = this.element.offsetTop+1+'px';
+    }
+};
+
+
+function wiggle(direction) {
+    wiggle.pop();
+    wiggle.unshift(direction);
+}
+
 function keyDispatcher(key) {
-    // addBlock();
     // console.log(key);
     //invoked from html element
     switch (key.which) {
@@ -23,30 +48,14 @@ function go(where){
 
     // var obj = document.getElementById('user');
     wiggle[0] = where;
+    // console.log(wiggle);
     var obj = body;
 
-    // var obj = body[0];
-    //     var obj = ;
         switch (where) {
             case '-' : if (step > 1 ) step-=1; break;
             case '=' : if (step < 20) step+=1; break;
             default : smoothMove(where, obj); break;
         }
-
-    // if (body.length > 1){follow(info.direction)}
-
-
-    // switch (where) {
-    //     case '-' : if (step > 1 ) step-=1; break;
-    //     case '=' : if (step < 20) step+=1; break;
-    //     default : smoothMove(where, obj); break;
-    // }
-}
-
-function follow(direction) {
-
-    var obj = body[1];
-    smoothMove(direction, obj);
 }
 
 function addBlock() {
@@ -57,27 +66,30 @@ function addBlock() {
     block.style.position = 'absolute';
 
     if (info.direction == 'right') {
-        block.style.top = body[body.length-1].style.top;
-        block.style.left = parseInt(body[body.length-1].style.left)-10;
+        block.style.top = body[body.length-1].element.style.top;
+        block.style.left = parseInt(body[body.length-1].element.style.left)-10;
     }
 
     if (info.direction == 'left') {
-        block.style.top = body[body.length-1].style.top;
-        block.style.left = parseInt(body[body.length-1].style.left)+10;
+        block.style.top = body[body.length-1].element.style.top;
+        block.style.left = parseInt(body[body.length-1].element.style.left)+10;
     }
 
     if (info.direction == 'up') {
-        block.style.top = parseInt(body[body.length-1].style.top) + 10;
-        block.style.left = body[body.length-1].style.left;
+        block.style.top = parseInt(body[body.length-1].element.style.top) + 10;
+        block.style.left = body[body.length-1].element.style.left;
     }
 
     if (info.direction == 'down') {
-        block.style.top = parseInt(body[body.length-1].style.top) - 10;
-        block.style.left = body[body.length-1].style.left;
+        block.style.top = parseInt(body[body.length-1].element.style.top) - 10;
+        block.style.left = body[body.length-1].element.style.left;
     }
 
     document.body.appendChild(block);
-    body.push(block);
+    var newCell = new Cell(block);
+    newCell.direction = body[body.length-1].direction;
+    newCell.index = body.length;
+    body.push(newCell);
     wiggle.push(wiggle[wiggle.length-1]);
 
     // smoothMove(info.direction, block);
@@ -93,50 +105,123 @@ function stopMoving(){
 function smoothMove(direction, obj) {
     stopMoving();
     info.direction = direction;
+    var i = 0;
+    body[0].direction = direction;
+
 
     if (direction == 'left') {
+        var cnt = 0;
         var interval = setInterval(function () {
-            obj.forEach(function(obj){
-                obj.style.left = obj.offsetLeft-1+'px'
+            if (cnt == 10) {
+                cnt = 0;
+                wiggle.pop();
+                wiggle.unshift(direction);
+            } else {cnt++; }
+
+            obj.forEach(function (obj) {
+                if (obj.direction != direction && obj.cnt < 10) {
+                    if (obj.direction == 'right') obj.moveRight();
+                    if (obj.direction == 'left') obj.moveLeft();
+                    if (obj.direction == 'up') obj.moveUp();
+                    if (obj.direction == 'down') obj.moveDown();
+                    obj.cnt++;
+                }
+
+                else if (obj.direction != direction && obj.cnt == 10) {
+                    // obj.direction = direction;
+                    obj.direction = wiggle[obj.index];
+                    obj.moveLeft();
+                    obj.cnt = 0;
+                } else { obj.moveLeft(); }
             });
-            // obj.style.left = obj.offsetLeft-1+'px';
         }, 10);
         active.push(interval);
     }
 
     if (direction == 'right') {
+        var cnt = 0;
+
         var interval = setInterval(function () {
-            obj.forEach(function(obj){
-                obj.style.left = obj.offsetLeft+1+'px'
+            if (cnt == 10) {
+                cnt = 0;
+                wiggle.pop();
+                wiggle.unshift(direction);
+            } else {cnt++; }
+
+            obj.forEach(function (obj) {
+                if (obj.direction != direction && obj.cnt < 10) {
+                    if (obj.direction == 'right') obj.moveRight();
+                    if (obj.direction == 'left') obj.moveLeft();
+                    if (obj.direction == 'up') obj.moveUp();
+                    if (obj.direction == 'down') obj.moveDown();
+                    obj.cnt++;
+                }
+
+                else if (obj.direction != direction && obj.cnt == 10) {
+                    obj.direction = wiggle[obj.index];
+                    obj.moveRight();
+                    obj.cnt = 0;
+                } else { obj.moveRight(); }
             });
+
         }, 10);
         active.push(interval);
     }
 
     if (direction == 'up') {
+        var cnt = 0;
         var interval = setInterval(function () {
-            obj.forEach(function(obj){
-                obj.style.top = obj.offsetTop-1+'px';
+            if (cnt == 10) {
+                cnt = 0;
+                wiggle.pop();
+                wiggle.unshift(direction);
+            } else {cnt++; }
+
+            obj.forEach(function (obj) {
+                if (obj.direction != direction && obj.cnt < 10) {
+                    if (obj.direction == 'right') obj.moveRight();
+                    if (obj.direction == 'left') obj.moveLeft();
+                    if (obj.direction == 'up') obj.moveUp();
+                    if (obj.direction == 'down') obj.moveDown();
+                    obj.cnt++;
+                }
+
+                else if (obj.direction != direction && obj.cnt == 10) {
+                    obj.direction = wiggle[obj.index];
+                    obj.moveUp();
+                    obj.cnt = 0;
+                } else { obj.moveUp(); }
             });
-            // obj.style.top = obj.offsetTop-1+'px';
         }, 10);
         active.push(interval);
     }
 
     if (direction == 'down') {
+        var cnt = 0;
         var interval = setInterval(function () {
+            if (cnt == 10) {
+                cnt = 0;
+                wiggle.pop();
+                wiggle.unshift(direction);
+            } else {cnt++; }
 
-            /*
-                1) read[0] wiggle - draw
-                2) read[1] wiggle - 
-                2) draw others according to wiggle and assign second ele wiggle down
-             */
+            obj.forEach(function (obj) {
+                if (obj.direction != direction && obj.cnt < 10) {
+                    if (obj.direction == 'right') obj.moveRight();
+                    if (obj.direction == 'left') obj.moveLeft();
+                    if (obj.direction == 'up') obj.moveUp();
+                    if (obj.direction == 'down') obj.moveDown();
+                    obj.cnt++;
+                }
 
-            obj.forEach(function(obj){
-                obj.style.top = obj.offsetTop+1+'px';
+                else if (obj.direction != direction && obj.cnt == 10) {
+                    obj.direction = wiggle[obj.index];
+                    obj.moveDown();
+                    obj.cnt = 0;
+                } else { obj.moveDown(); }
             });
-            // obj.style.top = obj.offsetTop+1+'px';
         }, 10);
         active.push(interval);
     }
 }
+
